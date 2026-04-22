88d9cf54e8de2e5661637d92c9e156d8bb66aa64
SonamSherap10 <sonamserpa3231@.com>
2026-04-22 13:20:27 +0545

    added crud functionality through socket

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,6 @@ import express from 'express'
 const app = express()
 import ("../src/todo/todoController.js")
 
-
+app.set("view engine","ejs")
 
 export default app
--- src/todo/todoController.ts
+++ src/todo/todoController.ts
@@ -1,6 +1,7 @@
 import { Socket } from "socket.io";
 import { getServer } from "../../server.js";
 import todoModel from "./todoModel.js";
+import { Status, type Itodo } from "./todoTypes.js";
 
 class todo{
     private io = getServer();
@@ -8,10 +9,12 @@ class todo{
         this.io.on("connection",(socket)=>{
             console.log("socket connection has been established")
             socket.on("addTodo",(data)=>this.handleAddTodo(socket,data))
+            socket.on("deleteTodo",(data)=>{this.handleDeleteTodo(socket,data)})
+            socket.on("updateTodo",(data)=>{this.handleUpdateTodo(socket,data)})
         })
        
     }
-    private async handleAddTodo(socket:Socket,data:any){
+    private async handleAddTodo(socket:Socket,data:Itodo){
    try {
           const{task,deadline,status} =data
      await todoModel.create({
@@ -31,9 +34,55 @@ class todo{
    }
     }
 
-    private async handleDeleteTodo(socket:Socket,data:{id:string}){
-        
+    private async handleDeleteTodo(socket:Socket,data:{id:string}){ 
+          try {
+              const {id} = data
+        const deleteTodo = await todoModel.findByIdAndDelete(id)
+        if(!deleteTodo){
+            socket.emit("todoResponse",{
+                status:"error",
+                message:"Cannot delete the task"
+            })
+            return;
+        }
+           const alltodos= await todoModel.find()
+         socket.emit("todoResponse",{
+           message:"Deleted successfully ",
+           alltodos
+         })
+          } catch (error) {
+            socket.emit("todoResponse",{
+                message:"Cannot delete the task",
+                error
+            })
+          }
     }
+
+    private async handleUpdateTodo(socket:Socket,data:{id:string,status:Status}){
+     try {
+             const{id,status}= data
+         const update = await todoModel.findByIdAndUpdate(id,{status})
+         if(!todo){
+            socket.emit("todoResponse",{
+                status:"error",
+                message:"todo not found"
+            })
+            return;
+         }
+          const alltodos= await todoModel.find({status:Status.Pending})
+         socket.emit("todoResponse",{
+           message:"Updated successfully ",
+           alltodos
+         })
+
+     } catch (error) {
+          socket.emit("todoResponse",{
+                error,
+                message:"error has occured "
+            })
+     }
+    }
+
 }
  
 export default new todo()
