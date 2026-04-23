b92b99b5a5349b5099c6782e956d7fc874fcd0ad
Sonam Sherpa <sonamserpa3231@gmail.com>
2026-04-23 14:39:40 +0545

    Remove unnecessary comment in app.ts

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
-app.set("views", path.join(__dirname, "views")); // ← remove the ../
+app.set("views", path.join(__dirname, "views")); 
 app.set("view engine", "ejs");
  
 app.get("/", (req, res ) => {
